8b864c44c0c56929c2287c8e27474ac88f672a4f
Robotz213 <nicholas@robotz.dev>
2025-05-26 16:58:59 -0300

    chore: update configuration files for improved formatting and environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly API_REST_URL: string
+  readonly VITE_SOCKET_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
