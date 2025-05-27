3be04b77e3073ef0e58f75e0a60d324254c5397a
Robotz213 <nicholas@robotz.dev>
2025-05-27 17:24:21 -0300

    feat: add LogoExample component and InputView component with password visibility toggle
    - Created LogoExample.vue to display the favicon image.
    - Introduced InputView.vue for styled input fields with support for icons and password visibility toggle.

---
+++ src/views/home/components/HeaderView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import LogoExample from "./icons/LogoExample.vue";
+</script>
+
+<template>
+  <header>
+    <div class="container mt-5">
+      <div class="d-grid justify-content-center align-items-center">
+        <div class="d-flex justify-content-center">
+          <div class="shadow-lg p-2 mb-2 mt-auto bg-body-tertiary rounded rounded-circle">
+            <LogoExample />
+          </div>
+        </div>
+        <div class="card-title d-grid justify-content-center align-items-center">
+          <span class="fw-bold text-center fs-3 text"> Dusdoc </span>
+          <span class="text-body-tertiary text-center fw-semibold">
+            Sistema de Gestão de Documentos
+          </span>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/views/home/components/icons/LogoExample.vue
@@ -0,0 +1,3 @@
+<template>
+  <img width="49" src="@/assets/img/favicon.png" alt="" class="rounded-4" />
+</template>
