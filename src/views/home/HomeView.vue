b2258c6cc1373a0d398e7db0055a612a794405bc
Robotz213 <nicholas@robotz.dev>
2025-05-28 10:45:31 -0300

    Update theme colors and add ListDocsView component to HomeView
    - Changed dark theme background color from $blue-chill-900 to $blue-chill-700.
    - Changed light theme background color from $blue-chill-600 to $blue-chill-400.
    - Imported and included ListDocsView component in HomeView.vue.
    - Created ListDocsView.vue with a layout for recent documents.

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import CardEmployee from "./components/CardEmployee.vue";
+import ListDocsView from "./components/ListDocsView.vue";
 </script>
 
 <template>
   <div class="p-2">
     <CardEmployee />
+    <ListDocsView />
   </div>
 </template>
---
+++ src/views/home/components/ListDocsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-5" id="files">
+    <div class="d-flex gap-0 justify-content-between align-items-center">
+      <span class="ms-1 fs-5 fw-semibold">Documentos recentes</span>
+      <a class="text-primary fw-semibold link-offset-3" href="#">Ver todos</a>
+    </div>
+    <div class="card mt-2">
+      <ul class="list-group">
+        <li class="list-group-item" style="height: 8.7dvh">An item</li>
+        <li class="list-group-item" style="height: 8.7dvh">A second item</li>
+        <li class="list-group-item" style="height: 8.7dvh">A third item</li>
+      </ul>
+    </div>
+  </div>
+</template>
