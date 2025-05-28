fbff499d3a1a8deb89261079ab14e58724ef7ab3
Robotz213 <nicholas@robotz.dev>
2025-05-28 19:57:40 -0300

    feat: add responsive height adjustment on window resize and improve layout styling

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
 import "@/assets/scripts/color-modes.js";
 import FooterView from "./components/FooterView.vue";
 import HeaderView from "./components/HeaderView.vue";
+import { ref } from "vue";
+
+const HeigthWindow = ref("1080px");
+
+const handleResize = () => {
+  HeigthWindow.value = `${window.innerHeight - 300}px`;
+};
+
+window.addEventListener("resize", handleResize);
 </script>
 
 <template>
@@ -14,7 +23,7 @@ import HeaderView from "./components/HeaderView.vue";
       >
         <component :is="Component" />
       </div>
-      <div v-else class="d-flex flex-column gap-2" style="height: 100vh">
+      <div v-else class="d-flex flex-column gap-2 overflow-y-auto" style="height: 100dvh">
         <HeaderView class="mb-auto" />
         <main>
           <component :is="Component" />
