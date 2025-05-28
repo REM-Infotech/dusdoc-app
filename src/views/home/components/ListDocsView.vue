78f83b62c0015b39fffa131fc413876b5cac5f32
Robotz213 <nicholas@robotz.dev>
2025-05-28 11:40:16 -0300

    feat: update ListDocsView with new FontAwesome icons and enhance document item structure

--- src/views/home/components/ListDocsView.vue
+++ src/views/home/components/ListDocsView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { faFile } from "@fortawesome/free-regular-svg-icons";
+import {
+  faFileInvoice,
+  faDownload,
+  faIdCard,
+  faFileContract,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 </script>
 
@@ -14,14 +19,57 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
         <li class="list-group-item">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex justify-content-start align-items-center">
-              <div class="p-2 bg-primary bg-opacity-25 rounded">
-                <FontAwesomeIcon :icon="faFile" size="3x" color="white" />
+              <div class="p-2 bg-primary rounded">
+                <FontAwesomeIcon :icon="faFileInvoice" size="lg" color="white" />
               </div>
+              <div class="d-grid gap-0 ms-2">
+                <span class="fw-semibold" style="font-size: 0.9rem">Contracheque Maio/2025</span>
+                <span class="text-secondary fw-semibold" style="font-size: 0.8rem">
+                  Enviado em 21/05/2024
+                </span>
+              </div>
+            </div>
+            <div class="bg-opacity-25 rounded">
+              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
             </div>
           </div>
         </li>
-        <li class="list-group-item" style="height: 8.7dvh">A second item</li>
-        <li class="list-group-item" style="height: 8.7dvh">A third item</li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex justify-content-start align-items-center">
+              <div class="p-1 bg-success rounded">
+                <FontAwesomeIcon :icon="faIdCard" size="lg" color="white" />
+              </div>
+              <div class="d-grid gap-0 ms-2">
+                <span class="fw-semibold" style="font-size: 0.9rem">Carteira Identificação</span>
+                <span class="text-secondary fw-semibold" style="font-size: 0.8rem">
+                  Atualizado em 21/05/2024
+                </span>
+              </div>
+            </div>
+            <div class="bg-opacity-25 rounded">
+              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
+            </div>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex justify-content-start align-items-center">
+              <div class="p-2 bg-warning rounded" style="">
+                <FontAwesomeIcon :icon="faFileContract" size="lg" color="white" />
+              </div>
+              <div class="d-grid gap-0 ms-2">
+                <span class="fw-semibold" style="font-size: 0.9rem">Contrato de Experiência</span>
+                <span class="text-secondary fw-semibold" style="font-size: 0.8rem">
+                  Atualizado em 21/05/2024
+                </span>
+              </div>
+            </div>
+            <div class="bg-opacity-25 rounded">
+              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
