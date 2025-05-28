f8645d9732b89a0807870ae168ebb8c9b6676fdb
Robotz213 <nicholas@robotz.dev>
2025-05-28 10:57:06 -0300

    feat: enhance ListDocsView with FontAwesome icon and update item structure

--- src/views/home/components/ListDocsView.vue
+++ src/views/home/components/ListDocsView.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { faFile } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>
+
 <template>
   <div class="mt-5" id="files">
     <div class="d-flex gap-0 justify-content-between align-items-center">
@@ -6,7 +11,15 @@
     </div>
     <div class="card mt-2">
       <ul class="list-group">
-        <li class="list-group-item" style="height: 8.7dvh">An item</li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex justify-content-start align-items-center">
+              <div class="p-2 bg-primary bg-opacity-25 rounded">
+                <FontAwesomeIcon :icon="faFile" size="3x" color="white" />
+              </div>
+            </div>
+          </div>
+        </li>
         <li class="list-group-item" style="height: 8.7dvh">A second item</li>
         <li class="list-group-item" style="height: 8.7dvh">A third item</li>
       </ul>
