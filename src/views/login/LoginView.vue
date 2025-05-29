a3d01c7542a8f29146d2645c186e3694274989b4
Robotz213 <nicholas@robotz.dev>
2025-05-29 20:05:50 -0300

    style: enhance layout of LoginView component for improved responsiveness

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -22,7 +22,9 @@ function handleSubmit(event: Event) {
 </script>
 
 <template>
-  <div class="login-card d-flex flex-column gap-3 shadow-lg me-3 ms-3 p-3 rounded rounded-4">
+  <div
+    class="mt-auto mb-auto login-card d-flex flex-column gap-3 shadow-lg me-3 ms-3 p-3 rounded rounded-4"
+  >
     <div class="d-flex gap-4 justify-content-center align-items-center">
       <div class="d-flex flex-column gap-2 justify-content-center align-items-center">
         <IconExample />
