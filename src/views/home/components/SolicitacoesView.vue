b3a0a1de5b943d6bca4755c22f9b72f447969cf2
Robotz213 <nicholas@robotz.dev>
2025-05-30 14:33:00 -0300

    style: update SolicitacoesView with new document statuses and improved layout

--- src/views/home/components/SolicitacoesView.vue
+++ src/views/home/components/SolicitacoesView.vue
@@ -1,71 +1,98 @@
 <script setup lang="ts">
-import { StyledDiv } from "@/components/styled";
 import {
   faFileInvoice,
-  faDownload,
   faIdCard,
   faFileContract,
+  faStopwatch,
+  faCheckCircle,
+  faWarning,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { StyledDiv } from "@/components/styled";
+import { ref } from "vue";
+
+const arquivos = ref(true);
 </script>
 
 <template>
-  <StyledDiv id="files">
+  <StyledDiv class="mt-2" id="files">
     <div class="d-flex gap-0 justify-content-between align-items-center">
-      <span class="ms-1 fs-5 fw-semibold">Documentos recentes</span>
-      <a class="text-primary fw-semibold link-offset-3" href="#">Ver todos</a>
+      <span class="ms-1 fw-semibold">Documentos Solicitados</span>
+      <a class="fw-semibold link-offset-3" href="#">Ver todos</a>
     </div>
     <div class="card mt-2">
-      <ul class="list-group">
-        <li class="list-group-item">
+      <TransitionGroup class="list-group" name="list" tag="ul">
+        <li class="list-group-item p-2" v-if="arquivos">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex justify-content-start align-items-center">
               <div class="p-2 bg-primary rounded">
                 <FontAwesomeIcon :icon="faFileInvoice" size="lg" color="white" />
               </div>
               <div class="d-grid gap-0 ms-2">
-                <span class="fw-semibold">Contracheque Maio/2025</span>
-                <span class="text-secondary fw-semibold"> Enviado em 21/05/2024 </span>
+                <span class="fw-semibold">Certidão CDI</span>
+                <span class="text-secondary fw-semibold"> Solicitado em 21/02/2024 </span>
               </div>
             </div>
-            <a href="#" class="bg-opacity-25 rounded">
-              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
-            </a>
+            <div
+              class="bg-opacity-10 p-1 bg-success text-decoration-none link-body-emphasis rounded d-flex justify-content-center align-items-center gap-2"
+            >
+              <FontAwesomeIcon :icon="faCheckCircle" size="1x" class="text-success" />
+              <span class="text-success" style="font-size: 0.8rem">Concluido</span>
+            </div>
           </div>
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item p-2" v-if="arquivos">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex justify-content-start align-items-center">
               <div class="p-1 bg-success rounded">
                 <FontAwesomeIcon :icon="faIdCard" size="lg" color="white" />
               </div>
               <div class="d-grid gap-0 ms-2">
-                <span class="fw-semibold">Carteira Identificação</span>
-                <span class="text-secondary fw-semibold"> Atualizado em 21/05/2024 </span>
+                <span class="fw-semibold">Identidade (RG/CNH)</span>
+                <span class="text-secondary fw-semibold"> Solicitado em 21/02/2024 </span>
               </div>
             </div>
-            <a href="#" class="bg-opacity-25 rounded">
-              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
-            </a>
+            <div
+              class="bg-opacity-10 p-1 bg-primary text-decoration-none link-body-emphasis rounded d-flex justify-content-center align-items-center gap-2"
+            >
+              <FontAwesomeIcon :icon="faStopwatch" size="1x" class="text-primary" />
+              <span class="text-primary" style="font-size: 0.8rem">Analisando</span>
+            </div>
           </div>
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item p-2" v-if="arquivos">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex justify-content-start align-items-center">
               <div class="p-2 bg-warning rounded" style="">
                 <FontAwesomeIcon :icon="faFileContract" size="lg" color="white" />
               </div>
               <div class="d-grid gap-0 ms-2">
-                <span class="fw-semibold">Contrato de Experiência</span>
-                <span class="text-secondary fw-semibold"> Atualizado em 21/05/2024 </span>
+                <span class="fw-semibold">Comprovante de Residência</span>
+                <span class="text-secondary fw-semibold"> Solicitado em 21/02/2024 </span>
               </div>
             </div>
-            <a href="#" class="bg-opacity-25 rounded">
-              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
-            </a>
+            <div
+              class="bg-opacity-10 p-1 bg-warning text-decoration-none link-body-emphasis rounded d-flex justify-content-center align-items-center gap-2"
+            >
+              <FontAwesomeIcon :icon="faWarning" size="1x" class="text-warning" />
+              <span class="text-warning" style="font-size: 0.8rem">Pendente</span>
+            </div>
           </div>
         </li>
-      </ul>
+        <li class="list-group-item p-2" v-else>Sem Arquivos</li>
+      </TransitionGroup>
     </div>
   </StyledDiv>
 </template>
+
+<style lang="css" scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
