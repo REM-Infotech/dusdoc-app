27e025e3a1a3999a1bbd380432563acdd8cdf2c9
Robotz213 <nicholas@robotz.dev>
2025-05-30 13:33:47 -0300

    style: update CardSolicitacao component layout for improved carousel functionality and aesthetics

--- src/views/home/components/CardSolicitacao.vue
+++ src/views/home/components/CardSolicitacao.vue
@@ -3,21 +3,33 @@ import { StyledDiv } from "@/components/styled";
 </script>
 
 <template>
-  <StyledDiv
-    id="carouselExampleSlidesOnly"
-    class="carousel rounded rounded-4"
-    data-bs-ride="carousel"
-  >
-    <div class="carousel-inner bg-primary">
+  <StyledDiv id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner bg-primary rounded rounded-4">
       <div class="carousel-item active bg-blue">
         <div class="card bg-primary" style="height: 200px">teste</div>
       </div>
       <div class="carousel-item bg-blue">
-        <div class="card bg-primary" style="height: 200px"></div>
+        <div class="card bg-success" style="height: 200px"></div>
       </div>
       <div class="carousel-item bg-blue">
-        <div class="card bg-primary" style="height: 200px"></div>
+        <div class="card bg-warning" style="height: 200px"></div>
       </div>
     </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="prev"
+    >
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="next"
+    >
+      <span class="visually-hidden">Next</span>
+    </button>
   </StyledDiv>
 </template>
