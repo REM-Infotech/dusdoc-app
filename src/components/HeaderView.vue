9328c1f756793337c8354248335ca49f474ff9da
Robotz213 <nicholas@robotz.dev>
2025-05-30 13:27:21 -0300

    style: enhance HeaderView navigation with rounded corners and shadow for improved aesthetics

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-bottom-4 shadow-lg">
       <div class="container-fluid justify-content-between">
         <a class="navbar-brand" href="#">DusDoc</a>
         <IconExample />
