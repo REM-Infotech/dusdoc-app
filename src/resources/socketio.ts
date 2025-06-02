bd66442b19013dae502a2a709060c20337c886a7
Robotz213 <nicholas@robotz.dev>
2025-06-02 19:34:54 -0300

    fix: change token storage from sessionStorage to localStorage in socketio.ts; ensure socket connection is established

--- src/resources/socketio.ts
+++ src/resources/socketio.ts
@@ -3,9 +3,11 @@ import io from "socket.io-client";
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"],
   extraHeaders: {
-    "HTTP-AUTHORIZATION": `Bearer ${sessionStorage.getItem("token") || ""}`,
+    "HTTP-AUTHORIZATION": `Bearer ${localStorage.getItem("token") || ""}`,
     "Content-Type": "application/json",
   },
 });
+
+socket.connect();
 
 export default socket;
--- src/stores/socketio.ts
+++ src/stores/socketio.ts
@@ -1,3 +1,11 @@
+import { io as sio } from "socket.io-client";
 
+const io = sio(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"],
+  autoConnect: true,
+  query: {
+    token: localStorage.getItem("token") || "",
+  },
+});
 
-
+export default io;
