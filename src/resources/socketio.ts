cffac07732317982317cfa6ec10b6c32019851ad
Robotz213 <nicholas@robotz.dev>
2025-06-02 17:13:14 -0300

    feat: add socket.io client setup for real-time communication

---
+++ src/resources/socketio.ts
@@ -0,0 +1,12 @@
+import io from "socket.io-client";
+
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"],
+  autoConnect: false,
+  extraHeaders: {
+    Authentication: `Bearer ${localStorage.getItem("token") || ""}`,
+    "Content-Type": "application/json",
+  },
+});
+
+export default socket;
