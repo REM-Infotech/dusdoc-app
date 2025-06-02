c4df993a6e9b933a6331358171a9ee96e69014ce
Robotz213 <nicholas@robotz.dev>
2025-06-02 12:58:54 -0300

    style: refactor ListDocsView and SolicitacoesView to improve transition styles and structure

--- src/views/documents/components/ListDocsView.vue
+++ src/views/documents/components/ListDocsView.vue
@@ -18,8 +18,14 @@ const arquivos = ref(true);
       <span class="ms-1 fw-semibold">Documentos recentes</span>
       <a class="fw-semibold link-offset-3" href="#" @click="arquivos = !arquivos">Ver todos</a>
     </div>
-    <div class="card mt-2 overflow-x-hidden" >
-      <TransitionGroup class="list-group overflow-y-auto overflow-x-hidden" name="list" tag="ul" mode="out-in" style="height: 200px;">
+    <div class="card mt-2 overflow-x-hidden">
+      <TransitionGroup
+        class="list-group overflow-y-auto overflow-x-hidden"
+        name="list"
+        tag="ul"
+        mode="out-in"
+        :style="{ 'height: 200px': !arquivos }"
+      >
         <li class="list-group-item p-2" v-if="!arquivos">
           <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex justify-content-start align-items-center">
@@ -44,22 +50,6 @@ const arquivos = ref(true);
               </div>
               <div class="d-grid gap-0 ms-2">
                 <span class="fw-semibold">Carteira Identificação</span>
-                <span class="text-secondary fw-semibold"> Atualizado em 21/05/2024 </span>
-              </div>
-            </div>
-            <a href="#" class="bg-opacity-25 rounded">
-              <FontAwesomeIcon :icon="faDownload" size="2x" color="blue" />
-            </a>
-          </div>
-        </li>
-        <li class="list-group-item p-2" v-if="!arquivos">
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="d-flex justify-content-start align-items-center">
-              <div class="p-2 bg-warning rounded" style="">
-                <FontAwesomeIcon :icon="faFileContract" size="lg" color="white" />
-              </div>
-              <div class="d-grid gap-0 ms-2">
-                <span class="fw-semibold">Contrato de Experiência</span>
                 <span class="text-secondary fw-semibold"> Atualizado em 21/05/2024 </span>
               </div>
             </div>
