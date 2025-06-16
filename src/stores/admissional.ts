d9a151b0c220e9a4101379cf897cd52c7444b269
Robotz213 <nicholas@robotz.dev>
2025-06-16 14:44:42 -0300

    feat: add age validation for data_nascimento in AdmissionalForm store

--- src/stores/admissional.ts
+++ src/stores/admissional.ts
@@ -1,6 +1,22 @@
 import { defineStore } from "pinia";
 import { reactive, watch } from "vue";
 
+function isOver14(dateString: string) {
+  const birthDate = new Date(dateString);
+  const today = new Date();
+
+  // Calcula a diferença de anos
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+
+  // Ajusta se ainda não fez aniversário este ano
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age >= 14;
+}
+
 const admissionalStore = defineStore("formAdmissional", () => {
   const AdmissionalForm = reactive({
     nome: "",
@@ -28,6 +44,15 @@ const admissionalStore = defineStore("formAdmissional", () => {
       .replace(/\D/g, "") // Remove tudo que não for dígito
       .replace(/^(\d{2})(\d{1})(\d{4})(\d{0,4}).*/, "($1) $2 $3-$4")
       .trim();
+    AdmissionalForm.cpf = newValue.cpf
+      .replace(/\D/g, "") // Remove tudo que não for dígito
+      .replace(/^(\d{3})\.?(\d{3})\.?(\d{3})-?(\d{2})/, "$1.$2.$3-$4")
+      .trim();
+
+    if (newValue.data_nascimento && !isOver14(newValue.data_nascimento)) {
+      alert("Insira uma data de nascimento válida!");
+      AdmissionalForm.data_nascimento = "";
+    }
   });
 
   return {
